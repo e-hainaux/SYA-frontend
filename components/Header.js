e6a8ae35d3d85f668abb97e15423044643ecdc07
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-03-29 11:19:15 +0100

    mise à jour Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../styles/Header.module.css";
 import Image from "next/image";
-import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 
 import logoSYA from "../public/images/logoSYA.svg";
 
 function Header() {
-  const [isHoveredSara, setIsHoveredSara] = useState(false);
-  const [isHoveredPrestations, setIsHoveredPrestations] = useState(false);
-  const [isHoveredTemoignages, setIsHoveredTemoignages] = useState(false);
-  const [isHoveredActus, setIsHoveredActus] = useState(false);
-  const [isHoveredContact, setIsHoveredContact] = useState(false);
-
   return (
     <div className={styles.headerGlobal}>
       <div className={styles.headerMainContainer}>
@@ -26,74 +19,6 @@ function Header() {
           />
         </div>
         <q>Des pauses dans la pose guidées par quelques proses.</q>
-        {/* <nav className={styles.nav}>
-          <ul className={styles.ul}>
-            <ScrollLink
-              className={`${isHoveredSara ? styles.hovered : styles.a} ${
-                styles.pointer
-              }`}
-              to="PresentationSara"
-              smooth={false}
-              duration={800}
-              onMouseEnter={() => setIsHoveredSara(true)}
-              onMouseLeave={() => setIsHoveredSara(false)}
-            >
-              Sara
-            </ScrollLink>
-
-            <ScrollLink
-              className={`${isHoveredPrestations ? styles.hovered : styles.a} ${
-                styles.pointer
-              }`}
-              to="presentationYoga"
-              smooth={true}
-              duration={500}
-              onMouseEnter={() => setIsHoveredPrestations(true)}
-              onMouseLeave={() => setIsHoveredPrestations(false)}
-            >
-              Prestations
-            </ScrollLink>
-
-            <ScrollLink
-              className={`${isHoveredTemoignages ? styles.hovered : styles.a} ${
-                styles.pointer
-              }`}
-              to="temoignages"
-              smooth={true}
-              duration={500}
-              onMouseEnter={() => setIsHoveredTemoignages(true)}
-              onMouseLeave={() => setIsHoveredTemoignages(false)}
-            >
-              Témoignages
-            </ScrollLink>
-
-            <ScrollLink
-              className={`${isHoveredActus ? styles.hovered : styles.a} ${
-                styles.pointer
-              }`}
-              to="actus"
-              smooth={true}
-              duration={500}
-              onMouseEnter={() => setIsHoveredActus(true)}
-              onMouseLeave={() => setIsHoveredActus(false)}
-            >
-              Actus
-            </ScrollLink>
-
-            <ScrollLink
-              className={`${isHoveredContact ? styles.hovered : styles.a} ${
-                styles.pointer
-              }`}
-              to="contact"
-              smooth={true}
-              duration={500}
-              onMouseEnter={() => setIsHoveredContact(true)}
-              onMouseLeave={() => setIsHoveredContact(false)}
-            >
-              Contact
-            </ScrollLink>
-          </ul>
-        </nav> */}
       </div>
     </div>
   );
